d3fd9dfcf7cee1567fc8012d0dd8ccdefb7c4186
reshmakunwar17 <reshmakunwar17@gmail.com>
2025-10-25 09:52:11 +0545

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import './styles.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+])
+
+const root = createRoot(document.getElementById('root')!)
+root.render(<RouterProvider router={router} />)
